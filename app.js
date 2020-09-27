9667ae85a95a4f7263e6d2de8e6f8e8628f023f2
tosimi adesoye <tosimiadesoye111@gmail.com>
2020-09-27 10:09:09 +0100

    final update

--- app.js
+++ app.js
@@ -91,27 +91,28 @@ const  QUESTIONS = [
 
 let score = 0;
 let lengthArray = QUESTIONS.length;
-//let questionVar;
-//let answerVar;
+
+const swapQuestions = (array,a,b) => {
+	let c = array[a]
+	array[a] = array[b]
+	array[b] = c
+}
 
 //function to generate random questions
 const generateQuestions = () =>{
-
-    let randomQuestions = Math.floor((lengthArray - 1) * Math.random())
+    let randomQuestions = Math.floor((lengthArray) * Math.random())
     let shuffleQuestion = QUESTIONS[randomQuestions]
+    
 
-  /*if(randomQuestions == lengthArray){
-    lengthArray= lengthArray-1;
+  if(randomQuestions === lengthArray){      
+    lengthArray = lengthArray-1;
+
   }else{
-    swap(QUESTIONS[randomQuestions], QUESTIONS[lengthArray])
+    swapQuestions(QUESTIONS,randomQuestions, lengthArray)
     lengthArray = lengthArray-1;
   }
-*/
-   // swap(randomQuestions, )
-    
-    
+	console.log(QUESTIONS);
     return shuffleQuestion
-
 }
 
 
@@ -119,10 +120,9 @@ const generateQuestions = () =>{
 
 
 
-//function to display question
+//function to create questions
 
 let createQuestion = (shuffledQuestion) => {
-    //appSection.removeChild(div2)
     questionVar = document.createElement("div")
     questionVar.setAttribute("id", "question")  
     let question = document.createElement("p")
@@ -137,25 +137,25 @@ let createAnswers = (shuffledQuestion) => {
     answerVar = document.createElement("div")
     answerVar.setAttribute("id","answers")
     shuffledQuestion.answers.map((elem) => {
-    let button = document.createElement("button")
-    button.setAttribute("id", "answerButton") 
-    button.setAttribute("class", "ans")  
-    button.textContent = elem.text
-    answerVar.appendChild(button) 
+		let button = document.createElement("button")
+		button.setAttribute("id", "answerButton") 
+		button.setAttribute("class", "ans")  
+		button.textContent = elem.text
+		answerVar.appendChild(button) 
     
     //function for keeping track of the score, disabling the button once it's clicked and selecting which is right/wrong
     button.onclick = () => {
-        let btnClass = document.getElementsByClassName("ans")
-        for(let i = 0; i < btnClass.length; i++){
-       if (btnClass[i]){
-            btnClass[i].disabled = true
+		let btnClass = document.getElementsByClassName("ans")
+		for(let i = 0; i < btnClass.length; i++){
+		if (btnClass[i]){
+			btnClass[i].disabled = true
         }else{
            btnClass[i].disabled = ""
-    }
+		}
     }    
         if(elem.correct === true){
             score = score + 1
-              button.style.background = "green"         
+            button.style.background = "green"         
         }else{
             button.style.background = "red"             
         }   
@@ -176,13 +176,22 @@ let createScore = () =>{
     return scorediv;
 }
 
+const playersResult = () => {
+    let resultDiv = document.createElement("div")
+    let result = document.createElement("p")
+    result.className = "result"
+    result.textContent = score + ' out of ' + QUESTIONS.length;
+    resultDiv.appendChild(result)
+    return resultDiv
+}
+
+
 const createNextButton = () => {
     let div2 = document.createElement("div2")
     let nextButton = document.createElement("button")
     nextButton.setAttribute("class", "next-btn")
     nextButton.textContent = 'next'
     nextButton.onclick = displayQuestions
-    //nextButton.onclick = displayAnswers
     div2.appendChild(nextButton)
     return div2;
 }  
@@ -191,24 +200,22 @@ const createNextButton = () => {
 let displayQuestions = () => {
     let appSection = document.getElementById("app")
     appSection.innerHTML = '';
-    //if(lengthArray>=0){
-    let shuffledQuestion = generateQuestions()
-    let question = createQuestion(shuffledQuestion);
-    let answer = createAnswers(shuffledQuestion);
-    let nextButton = createNextButton();
-    let scorediv = createScore();
+    if(lengthArray>0){
+		let shuffledQuestion = generateQuestions()
+		let question = createQuestion(shuffledQuestion);
+		let answer = createAnswers(shuffledQuestion);
+		let nextButton = createNextButton();
+		let scorediv = createScore();
+    
 
-    //Saurav - In this function, you need to delete previous question, if there is any and display the next question.
+		//Saurav - In this function, you need to delete previous question, if there is any and display the next question.
+		appSection.appendChild(question).appendChild(answer).appendChild(nextButton).appendChild(scorediv)
+	}else{
+		let result = playersResult();
+		appSection.appendChild(result);
+	}
 
-   
-
-    appSection.appendChild(question).appendChild(answer).appendChild(nextButton).appendChild(scorediv);
-}//else{
-    //let scorediv = createScore();
-    //appSection.appendChild(scorediv);
-//}
-
-//}
+}
 
 
 
@@ -226,8 +233,6 @@ let displayStart = () =>{
     startButton.className = "start"
     startButton.textContent = "start"
     startButton.onclick = displayQuestions
-        
-   // startButton.onclick = displayAnswers 
     div.appendChild(h3)
     div.appendChild(p1)
     div.appendChild(p2)
@@ -235,13 +240,8 @@ let displayStart = () =>{
     //Find the Main div where we need to append the contents
     let appSection = document.getElementById("app")
     appSection.appendChild(div)
-  //  lengthArray = QUESTIONS.length -1;
+    lengthArray = QUESTIONS.length -1;
 
 }
 
  displayStart()
-
-
-
-
-
